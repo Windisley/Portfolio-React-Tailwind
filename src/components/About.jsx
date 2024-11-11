c694d3f6ba38a349dfcb5245f0e75a856d156b86
Windisley <windisleyl@gmail.com>
2024-11-11 15:29:36 -0300

    Sobre mim Estilizado

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,18 +4,39 @@ const About = ()=>{
     return(
         <section className="
         w-full min-h-dvh flex justify-center
-        items-center
+        items-center bg-secundary
         ">
-            <div>  
-                <div>
-                    <img src={foto34} alt="Minha Foto" />
+            <div className="
+             max-w-contain h-full p-4 flex
+             justify-between items-center gap-4
+
+            ">  
+                <div className="
+                w-72 min-w-40 h-72 min-h-40 rounded-full border-2 
+                 border-primarydark bg-primary flex justify-center items-center
+                 relative overflow-hidden shadow-shadowdark
+                ">
+                    <img src={foto34} alt="Minha Foto" className="
+                      absolute left-2/4 top-2/4
+                      translate-x-centralizeEixo
+                      translate-y-centralizeEixo
+                      object-contain
+                      w-full 
+                    "/>
                 </div>
-                <div>
-                    <h2>
+                <div className="
+                 w-2/4 h-full flex flex-col gap-4 justify-center
+                 items-center
+                ">
+                    <h2 className="
+                     text-center font-poppins-semibold text-xl text-primary capitalize
+                    ">
                         Sobre mim
                     </h2>
 
-                    <p>
+                    <p className="
+                     text-left w-full font-poppins text-primary capitalize
+                    ">
                         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, ducimus est, cupiditate 
                         perspiciatis optio possimus corrupti recusandae perferendis odit doloremque fugiat aperiam deserunt! 
                         Facere quis voluptate perferendis porro ea adipisci.
